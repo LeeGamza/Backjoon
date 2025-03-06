8a24eb9063854eb0770d6164e8d28ddad79cca96
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2025-03-06 09:31:55 +0900

    day01

---
+++ Day01/Day01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 서울의 오늘 날짜를 YYYY-MM-DD 형식으로 출력한다.
+        /*Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd"));*/
+        
+        /*Console.WriteLine("26");
+        Console.WriteLine("hoon3243");*/
+        
+        Console.WriteLine($"         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+    }
+}
